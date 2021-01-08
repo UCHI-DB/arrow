54b55ae66d081b2ff1bb717dbcae6e13f1ff46ca
githubsigmod2021 <githubsigmod2021@gmail.com>
2021-01-08 15:01:57 -0600

    Creating Verify Scripts

--- cpp/ssb_verify/q1_1.py
+++ cpp/ssb_verify/q1_1.py
@@ -1,11 +1,11 @@
-with open('lineorder.tbl') as lineorder:
+with open('/Users/harper/ssb/lineorder.tbl') as lineorder:
+    sum = 0
     for line in lineorder.read().splitlines():
         columns = line.split('|')
-        discount = int(columns(11))
-        quantity = int(columns(8))
-        orderdate = columns(5)
-        extendedprice = float(columns(9))
-        sum = 0
+        discount = int(columns[11])
+        quantity = int(columns[8])
+        orderdate = columns[5]
+        extendedprice = float(columns[9])
         if discount >= 1 and discount <= 3 and quantity < 25 and orderdate >= '19940101' and orderdate <= '19941231':
             sum += extendedprice * discount
     print(sum)
