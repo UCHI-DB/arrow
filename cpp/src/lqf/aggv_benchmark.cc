59c0cdf6b097da007b51cc5bc829d72abda270fa
Hao Jiang <harperjiang>
2021-01-17 05:57:08 -0600

    agg benchmark on columnar table

--- cpp/src/lqf/aggv_benchmark.cc
+++ cpp/src/lqf/aggv_benchmark.cc
@@ -27,39 +27,37 @@ void init() {
 
     auto mod = (int) (right_size / mod_ratio);
     for (int i = 0; i < left_size; ++i) {
-        auto val = abs(rand()) % mod;
-        (*leftRowWriter)[i][0] = val;
-        (*leftRowWriter)[i][1] = 0;
-        (*leftRowWriter)[i][2] = 0;
-        (*leftRowWriter)[i][3] = 0;
-        (*leftRowWriter)[i][4] = 0;
-        (*leftColWriter)[i][0] = val;
-        (*leftColWriter)[i][1] = 0;
-        (*leftColWriter)[i][2] = 0;
-        (*leftColWriter)[i][3] = 0;
-        (*leftColWriter)[i][4] = 0;
+        auto val = abs(rand()) % key_size;
+        (*rowWriter)[i][0] = val;
+        (*rowWriter)[i][1] = rand();
+        (*rowWriter)[i][2] = rand();
+        (*rowWriter)[i][3] = rand();
+        (*rowWriter)[i][4] = rand();
+        (*colWriter)[i][0] = val;
+        (*colWriter)[i][1] = rand();
+        (*colWriter)[i][2] = rand();
+        (*colWriter)[i][3] = rand();
+        (*colWriter)[i][4] = rand();
     }
 
-    auto rightBlock = rightTable_->allocate(right_size);
-    auto rightWriter = rightBlock->rows();
-    for (int i = 0; i < right_size; ++i) {
-        (*rightWriter)[i][0] = i;
-        (*rightWriter)[i][1] = 0;
-    }
 }
 
-static void HashJoinBenchmark_Row(benchmark::State &state) {
+static void AggBenchmark_Row(benchmark::State &state) {
     init();
+    function<vector<AggField *>()> aggFields = []() {
+        return vector<AggField *>{new DoubleSum(1)};
+    };
     for (auto _:state) {
-        HashJoin join(0, 0, new RowBuilder({JL(1), JL(2), JL(3), JL(4), JR(1)}));
-        size_ = join.join(*leftRowTable_, *rightTable_)->size();
+        HashLargeAgg agg(hasher, RowCopyFactory().field(F_REGULAR, 0, 0)->buildSnapshot(), aggFields);
+        auto agged = agg.agg(*withPart);
+        size_ = agged->size();
     }
 }
 
 //READ HASHMAP
 //QUERY BITMAP
 
-static void HashJoinBenchmark_Column(benchmark::State &state) {
+static void AggBenchmark_Column(benchmark::State &state) {
     init();
     for (auto _ : state) {
         HashColumnJoin join(0, 0, new ColumnBuilder({JL(1), JL(2), JL(3), JL(4), JR(1)}), true);
@@ -67,8 +65,8 @@ static void HashJoinBenchmark_Column(benchmark::State &state) {
     }
 }
 
-BENCHMARK(HashJoinBenchmark_Row);
+BENCHMARK(AggBenchmark_Row);
 // Note
-BENCHMARK(HashJoinBenchmark_Column);
+BENCHMARK(AggBenchmark_Column);
 
 BENCHMARK_MAIN();
